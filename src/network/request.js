b0d229659279f480f64de0c6ad3bbf61919f2dc1
Anabenda <56350175+Anabenda@users.noreply.github.com>
2021-01-18 08:21:23 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export function request(config) {
   // 1. 创建axios的实例对象
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:7878/api/m5',
+    baseURL: 'http://xxxxxxxxxxxxxxx',
     timeout: 5000
   })
 
